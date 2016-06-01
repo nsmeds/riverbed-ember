9347244696400902bad702d7c9b5e163dce404e0
nsmeds <niilosmeds@gmail.com>
2016-06-01 13:05:32 -0700

    Basic layout with ember has gotten pretty close. Going to try some refactoring and commit before I screw things up too bad.

--- app/router.js
+++ app/router.js
@@ -6,11 +6,13 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('about', { path: '/about' });
-  this.route('contact', { path: '/contact' });
-  this.route('subscribe', { path: '/subscribe' });
-  this.route('back-issues', { path: '/back-issues' });
-  this.route('submit', { path: '/submit' });
+  this.route('menu', function () {
+    this.route('about', { path: '/about' });
+    this.route('contact', { path: '/contact' });
+    this.route('subscribe', { path: '/subscribe' });
+    this.route('back-issues', { path: '/back-issues' });
+    this.route('submit', { path: '/submit' });
+  });
 });
 
 export default Router;
